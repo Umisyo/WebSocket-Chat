a2641f251c465a1766e614754e19fea279c37542
Umisyo <kusunokisouta@gmail.com>
2019-06-17 11:00:32 +0900

    fix:propsで受け取る型を修正

---
+++ nuxt/components/messageBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <figure class="media-left">
+      <p class="image is-64x64">
+        <img src="https://bulma.io/images/placeholders/128x128.png">
+      </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <p>
+                >
+                <strong>User: {{ message.user }}</strong>
+                <br>
+                {{ message.text }}
+                <br>
+                <small>
+                    <a>Like</a>
+                    <a>Reply</a>
+                    . {{ message.date }}
+                </small>
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: Object
+    }
+}
+</script>
+
